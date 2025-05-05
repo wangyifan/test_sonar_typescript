588eb9f5ebae5e3c9ec69afd79c8e46f49db2a81
Yifan Wang <yifan@alchemy.com>
2025-05-05 14:34:55 -0700

    add tests for mul

--- tests/math.add.test.ts
+++ tests/math.add.test.ts
@@ -1,4 +1,4 @@
-import { add, subtract } from '../src/math/math';
+import { add, multiply, subtract } from '../src/math/math';
 
 describe('add', () => {
   it('adds two positive numbers', () => {
@@ -35,5 +35,24 @@ describe('subtract', () => {
   it('subtracts zero', () => {
     expect(subtract(0, 5)).toBe(-5);
     expect(subtract(5, 0)).toBe(5);
+  });
+});
+
+describe('multiply', () => {
+  it('multiplies two positive numbers', () => {
+    expect(multiply(2, 3)).toBe(6);
+  });
+
+  it('multiplies negative and positive number', () => {
+    expect(multiply(-2, 3)).toBe(-6);
+  });
+
+  it('multiplies two negative numbers', () => {
+    expect(multiply(-2, -3)).toBe(6);
+  });
+
+  it('multiplies by zero', () => {
+    expect(multiply(0, 5)).toBe(0);
+    expect(multiply(5, 0)).toBe(0);
   });
 });
