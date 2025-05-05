b4d777456dfe298fe73ade90cf0fc1455dfe3692
Yifan Wang <yifan@alchemy.com>
2025-05-05 14:26:48 -0700

    add tests for sub

--- tests/math.add.test.ts
+++ tests/math.add.test.ts
@@ -1,4 +1,4 @@
-import { add } from '../src/math/math';
+import { add, subtract } from '../src/math/math';
 
 describe('add', () => {
   it('adds two positive numbers', () => {
@@ -16,5 +16,24 @@ describe('add', () => {
   it('adds zero', () => {
     expect(add(0, 5)).toBe(5);
     expect(add(5, 0)).toBe(5);
+  });
+});
+
+describe('subtract', () => {
+  it('subtracts two positive numbers', () => {
+    expect(subtract(5, 3)).toBe(2);
+  });
+
+  it('subtracts negative and positive number', () => {
+    expect(subtract(-2, 3)).toBe(-5);
+  });
+
+  it('subtracts two negative numbers', () => {
+    expect(subtract(-2, -3)).toBe(1);
+  });
+
+  it('subtracts zero', () => {
+    expect(subtract(0, 5)).toBe(-5);
+    expect(subtract(5, 0)).toBe(5);
   });
 });
